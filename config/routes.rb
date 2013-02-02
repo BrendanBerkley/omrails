1a101cdf0df01162ab6c5081870ffb527960ec8e
Brendan Berkley <brendan.berkley@gmail.com>
2013-02-01 19:42:20 -0500

    Finished the third lecture in week on: adding pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Omrails::Application.routes.draw do
+ # get "pages/home"
+  
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
