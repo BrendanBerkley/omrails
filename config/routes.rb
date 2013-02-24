870b5323a3cbcea95420114149eaf8260749c442
Brendan Berkley <brendan.berkley@gmail.com>
2013-02-23 19:33:40 -0500

    Reroute root to pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Omrails::Application.routes.draw do
   
   get 'about' => 'pages#about'
 
-  root :to => 'pages#home'
+  root :to => 'pins#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
